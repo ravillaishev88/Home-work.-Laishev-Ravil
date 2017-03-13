7ba925fa90eeb011761bdd0fae58b5851e21704b
Ravil <ravillaishev88@gmail.com>
2017-03-13 20:41:30 +0600

    Third commit

---
+++ Zadanie3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Zadanie3 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите вещественное число!");
+        double s = scanner.nextDouble();
+        System.out.println("Округление: "+Math.round(s));
+    }
+}
