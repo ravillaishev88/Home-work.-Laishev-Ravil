0f483e03ed2bab87516fcb98729461633fd8e8cb
Ravil <ravillaishev88@gmail.com>
2017-03-16 14:14:00 +0600

    Пятый коммит 2

--- Massiv_1.java
+++ Massiv_1.java
@@ -5,11 +5,7 @@
 public class Massiv_1 {
     public static void main(String[] args) {
         int number = 11; // примитивный тип данных
-        String s = "Hello";
-        String s1 = new String ("Hello1"); // Ссылочный тип данных
         int [] numbers = new int [5]; // Cоздание целочислинного МАССИВА
-        System.out.println(numbers[1]);
-
-
+        
     }
 }
