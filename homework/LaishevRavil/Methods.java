3e609237ebd7e5fbb4b741d9ab28c197e359c27d
Ravil <ravillaishev88@gmail.com>
2017-03-17 21:34:36 +0600

    commit 2

---
+++ homework/LaishevRavil/Methods.java
@@ -0,0 +1,29 @@
+package homework.LaishevRavil;
+
+/**
+ * Created by User on 17.03.2017.
+ */
+public class ClassesObjects {
+    public static void main(String[] args) {
+        Person person1 = new Person();          // Создание объекта класса
+        person1.name = "Иван";
+        person1.age = 20;
+        person1.speak();
+        Person person2 = new Person();
+        person2.name = "Выся";
+        person2.age = 23;
+        person2.sayHello();                     // Вызов метода класса
+        person2.speak();                        // Вызов метода класса
+    }
+}
+class Person{
+    String name;
+    int age;
+
+    void speak(){                               // Создание метода класса
+        System.out.println("Меня зовут "+name+", и мне "+age+" лет.");
+    }
+    void sayHello(){                            // Создание метода класса
+        System.out.println("Привет");
+    }
+}
