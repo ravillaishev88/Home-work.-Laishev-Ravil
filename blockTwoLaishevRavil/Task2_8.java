85af53b30cc8d7e30445e79f774ae70dac14b33c
Ravil <ravillaishev88@gmail.com>
2017-03-19 16:31:56 +0600

    (2)

---
+++ blockTwoLaishevRavil/Task2_8.java
@@ -0,0 +1,10 @@
+package blockTwoLaishevRavil;
+public class Task2_8 {
+    public static void main(String[] args) {
+        int rez = 1;
+        for (int n = 0; n<20; n++) {
+            rez *= 2;
+            System.out.print(rez+" ");
+            }
+    }
+}
