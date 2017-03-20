f92c79f6ea7ce9ff7bf1f476a29624f9a1dbd3eb
Ravil <ravillaishev88@gmail.com>
2017-03-20 20:56:13 +0600

    Commit 2

--- blockThreeLaishevRavil/Task1.java
+++ blockThreeLaishevRavil/Task1.java
@@ -6,16 +6,16 @@ public class Task1 {
     public static void main(String[] args) {
         int[][] massiv = new int[8][5];
         Random rnd = new Random();
-        for (int i = 0; i < massiv.length; i++) {
-            for (int j = 0; j < massiv.length; j++) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 5; j++) {
                 massiv[i][j] = rnd.nextInt(89) + 10;
 
             }
 
         }
-        for (int i = 0; i < massiv.length; i++) {
-            for (int j = 0; j < massiv.length; j++) {
-                System.out.println(massiv[i][j] + " ");
+        for (int i = 0; i < 8; i++,System.out.println()) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(massiv[i][j] + " ");
             }
         }
     }
